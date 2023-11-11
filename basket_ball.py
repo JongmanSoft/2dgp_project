b99e8a3217d2f99e6fa99b121c523e2c51ab553b
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-11-11 19:54:24 +0900

    농구공 객체만들기
    어떻게 해줘야될까....

--- basket_ball.py
+++ basket_ball.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+from math import radians,cos,sin
 time = 0
 class goal:
     def __init__(self):
@@ -15,7 +15,22 @@ class goal:
         for s in self.sprite:
             s.draw(self.x,300)
 
-
+class ball:
+    def __init__(self):
+        self.sprite = load_image('resource/basketball.png')
+        self.x = 400
+        self.y = 100
+        self.z = 200
+        self.dir = 90
+        self.speed = 0
+        self.frame = 0
+    def update(self):
+        if (self.speed != 0):
+            self.frame+=1
+            self.frame = self.frame%6
+            self.x += cos(self.dir) *self.speed
+            self.y += sin(self.dir) *self.speed
+            self.z -= self.speed*10
 
 
 class basket_ball_scene:
