8a2a5ecda27c04bdd3824080d123783e40585417
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-11-11 18:40:31 +0900

    볼링씬 뼈대잡기

---
+++ bowling.py
@@ -0,0 +1,43 @@
+from pico2d import *
+
+
+class bowling_scene:
+    animation =0
+    run = 1
+    def __init__(self):
+        pass
+
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+    def handle_events(self):
+
+        events = get_events()
+        for event in events:
+            if event.type == SDL_MOUSEBUTTONDOWN and event.button ==1 :
+                pass
+            if event.type == SDL_MOUSEMOTION:
+                pass
+            if event.type == SDL_MOUSEBUTTONUP and event.button ==1:
+                pass
+
+
+    def running(self):
+        return self.run
+
+    def pause(self): pass
+
+    def resume(self): pass
+
