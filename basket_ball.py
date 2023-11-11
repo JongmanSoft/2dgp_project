85ee519db0e44be31ad2133b017e38dd707a221d
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-11-11 20:09:40 +0900

    공 움직이기
    공의 z 좌표에 따라 레이어가 바뀌게 출력

--- basket_ball.py
+++ basket_ball.py
@@ -12,8 +12,9 @@ class goal:
         if (self.x>650):self.dir = -1
         if (self.x <150):self.dir = 1
     def draw(self):
-        for s in self.sprite:
-            s.draw(self.x,300)
+        self.sprite[0].draw(self.x,300)
+    def ring_draw(self):
+        self.sprite[1].draw(self.x,300)
 
 class ball:
     def __init__(self):
@@ -22,15 +23,21 @@ class ball:
         self.y = 100
         self.z = 200
         self.dir = 90
-        self.speed = 0
+        self.speed = 1
         self.frame = 0
     def update(self):
         if (self.speed != 0):
-            self.frame+=1
+            self.frame += 1
             self.frame = self.frame%6
-            self.x += cos(self.dir) *self.speed
-            self.y += sin(self.dir) *self.speed
-            self.z -= self.speed*10
+            if (self.z > 80):
+                self.x += cos(self.dir) * self.speed * 15
+                self.y += sin(self.dir) * self.speed * 15
+                self.z -= self.speed*4
+            else:
+                self.y -= sin(self.dir) * self.speed * 10
+
+    def draw(self):
+        self.sprite.clip_composite_draw(100*self.frame,0,100,100,radians(self.dir-90),'0',self.x ,self.y ,self.z,self.z)
 
 
 class basket_ball_scene:
@@ -39,6 +46,7 @@ class basket_ball_scene:
     def __init__(self):
         self.back = load_image('resource/basketball_background.png')
         self.goal_dae = goal()
+        self.balls= [ball()]
         pass
 
 
@@ -53,11 +61,17 @@ class basket_ball_scene:
         global time
         time += 1
         self.goal_dae.update()
+        for b in self.balls:
+            b.update()
         pass
 
     def draw(self):
         self.back.draw(400,300)
         self.goal_dae.draw()
+        if (self.balls[0].z>80):self.goal_dae.ring_draw()
+        for b in self.balls:
+            b.draw()
+        if (self.balls[0].z <= 80): self.goal_dae.ring_draw()
         pass
 
 
@@ -66,6 +80,7 @@ class basket_ball_scene:
         events = get_events()
         for event in events:
             if event.type == SDL_MOUSEBUTTONDOWN and event.button ==1 :
+                print(self.balls[0].z)
                 pass
             if event.type == SDL_MOUSEMOTION:
                 pass
