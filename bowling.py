90f639cfc148f8d518473f69be555ec78e26fe0c
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-11-21 23:44:18 +0900

    볼링핀설치!!

--- bowling.py
+++ bowling.py
@@ -9,6 +9,7 @@ class my_ball:
         self.dir = 90
         self.frame = 0
         self.speed = 0
+        self.size = (200-(self.y/2))/2
     def draw(self):
         self.sprite.clip_composite_draw(self.frame*50,0,50,50,0, 'h',self.x,self.y,200-(self.y/2),200-(self.y/2))
     def update(self):
@@ -23,9 +24,31 @@ class my_ball:
 
         pass
 
+
+class pin:
+    def __init__(self,n):
+        if (n==0):self.size = 4
+        elif (n<3): self.size =3
+        elif (n<6): self.size =2
+        else : self.size = 1
+        add = 0
+        if (n ==0): add = 30
+        if (n != 0and n<3): add = 60
+        if (n >2 and n < 6): add = 50
+
+        self.x =add+ 280+self.size*50 + (n - (7-self.size)) *40
+        self.y = (4-self.size)*20 + 250
+        self.frame = 0
+        self.w =
+        self.h = 
 class pins:
     def __init__(self):
         self.sprite = load_image('resource/pin.png')
+        self.data = [pin(i)for i in range(10)]
+
+    def draw(self):
+        for p in range(9,-1,-1):
+            self.sprite.clip_composite_draw(self.data[p].frame*30,0,30,65,0,'h',self.data[p].x,self.data[p].y,(self.data[p].size+5)*6,(self.data[p].size+5)*13)
 
 
 class bowling_scene:
@@ -39,6 +62,7 @@ class bowling_scene:
         self.pin = load_image('resource/pin.png')
         self.arrow = load_image('resource/ball_dir.png')
         self.balls = [my_ball()]
+        self.Pin = pins()
         pass
 
 
@@ -63,6 +87,7 @@ class bowling_scene:
 
     def draw(self):
         self.back.draw(400,300)
+        self.Pin.draw()
         for ball in self.balls:
             ball.draw()
         if (self.state == 0):self.arrow.clip_composite_draw(0,0,100,290,radians(self.balls[0].dir-90),'h',400,75,100,290)
